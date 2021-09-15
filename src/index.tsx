67e6f55cf7602c8e73e14398b28517630c989c20
Danchenko <maksym.danchenko@sigmaukraine.com>
2021-09-15 17:41:25 +0300

    added gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import {App} from "./components/app/App"
-import "./styles/fonts.scss"
 import "./styles/null-styles.scss"
 import {BrowserRouter} from "react-router-dom"
 import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client"
